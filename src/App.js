91affd927424d4477ed27788732ca3b8637590a5
Hanndot <athaullahfarhan@gmail.com>
2022-10-25 15:39:01 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,38 @@
-import './App.css';
-import React from 'react';
-import Form from './component/Form';
-import Card from './component/Card';
+import "./App.css";
+
+import React from "react";
+import Form from "./component/Form";
+import Card from "./component/Card";
 
 function App() {
-  const [praktikan, setPraktikan] = React.useState(null);
-  const addPraktikanHandler = (data) => {
+  const [Mahasiswa, setMahasiswa] = React.useState(null);
+  const addMahasiswaHandler = (data) => {
     console.log(data);
-    setPraktikan(data);
+    setMahasiswa(data);
   };
-  const removePraktikanHandler = () => {
-    setPraktikan(null);
+  const removeMahasiswaHandler = () => {
+    setMahasiswa(null);
   };
   return (
     <div className="App">
-      <h1>Kartu Praktikan</h1>
-      <Form onAddPraktikan={addPraktikanHandler} />
+      <h1>Kartu Mahasiswa</h1>
+      <Form onAddMahasiswa={addMahasiswaHandler} />
       {/* Conditional rendering */}
-
-      {praktikan && (
+      {Mahasiswa && (
         <>
-          <button className="delete" onClick={removePraktikanHandler}>
+          <Card
+            nama_lengkap={Mahasiswa.nama_lengkap}
+            nama_panggilan={Mahasiswa.nama_panggilan}
+            nomor_telepon={Mahasiswa.nomor_telepon}
+            id_line={Mahasiswa.id_line}
+            tanggal_lahir={Mahasiswa.tanggal_lahir}
+            nim={Mahasiswa.nim}
+            email={Mahasiswa.email}
+            hobi={Mahasiswa.hobi}
+          />
+          <button className="delete" onClick={removeMahasiswaHandler}>
             Hapus
           </button>
-          <Card nama={praktikan.nama} kelompok={praktikan.kelompok} />
         </>
       )}
     </div>
